7b2d3e96dd45a73ed26b70598745880c69bcee94
black-ros3 <pohkeeheng@gmail.com>
2020-07-04 19:10:13 +0800

    push to heroku

---
+++ backend/build/precache-manifest.3954e181e6364cead64328e0e32349a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db855f0b794b9d20f162a9e52b7025ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2ecc8af8331f677b06f",
+    "url": "/static/css/main.8b92a585.chunk.css"
+  },
+  {
+    "revision": "d179369fe0b8db0a30b5",
+    "url": "/static/js/2.47b55f22.chunk.js"
+  },
+  {
+    "revision": "62be023d07e30b3b501f322b68296c6c",
+    "url": "/static/js/2.47b55f22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ecc8af8331f677b06f",
+    "url": "/static/js/main.2763f3f7.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
